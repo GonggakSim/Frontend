11c8ad31e3e6b6109e9db8dfd9b14e6fd2a9491a
KariV <156992938+KariV-824@users.noreply.github.com>
2025-01-16 23:16:00 +0900

    [Feat] 하단 내비게이션 바 추가
    하단 내비게이션 바 추가했습니다. 하단 바 구성요소에 맞게 프래그먼트도 임시로 추가해두었습니다.

---
+++ app/src/main/java/com/example/gonggaksim_frontend/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.gonggaksim_frontend
+
+import android.os.Bundle
+import android.os.Debug
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.collection.emptyLongSet
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.gonggaksim_frontend.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { view, insets ->
+            view.setPadding(0, 0, 0, insets.systemWindowInsetBottom)
+            insets.consumeSystemWindowInsets()
+        }
+
+        replaceFragment(TestFragment())
+
+        binding.btmNav.setOnItemSelectedListener{ item ->
+            when(item.itemId){
+                R.id.btm_test -> {
+                    replaceFragment(TestFragment())
+                    true
+                }
+                R.id.btm_cal -> {
+                    replaceFragment(CalenderFragment())
+                    true
+                }
+                R.id.btm_noti -> {
+                    replaceFragment(NotificationFragment())
+                    true
+                }
+                R.id.btm_my -> {
+                    replaceFragment(MypageFragment())
+                    true
+                }
+
+                else -> true
+            }
+
+
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, fragment)
+            .commit()
+    }
+
+}

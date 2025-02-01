e6887485981d5854d52e33b8303a209e2835219d
jam-jang <165284582+jam-jang@users.noreply.github.com>
2025-02-02 02:03:58 +0900

    [Feat] 로그아웃 팝업창 연결

--- app/src/main/java/com/example/gonggaksim_frontend/MypageFragment.kt
+++ app/src/main/java/com/example/gonggaksim_frontend/MypageFragment.kt
@@ -1,19 +1,31 @@
 package com.example.gonggaksim_frontend
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageButton
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MypageFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        val view = inflater.inflate(R.layout.fragment_mypage, container, false)
+
+        val logoutBtn = view.findViewById<View>(R.id.tvLogoutBtn) // 로그아웃 버튼
+
+        logoutBtn.setOnClickListener {
+            showLogoutDialog()
+        }
+
+        return view
+        //return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -32,9 +44,27 @@ class MypageFragment : Fragment() {
             val intent = Intent(requireContext(),MembershipWithdrawalActivity::class.java)
             startActivity(intent) //MembershipWithdrawalActivity 이동
         }
+    }
 
+    @SuppressLint("MissingInflatedId")
+    private fun showLogoutDialog() {
+        val dialog = BottomSheetDialog(requireContext())
+        val view = layoutInflater.inflate(R.layout.activity_dialog_logout, null)
 
+        val btnConfirm = view.findViewById<Button>(R.id.btnConfirm) // 로그아웃 버튼
+        val btnCancel = view.findViewById<ImageButton>(R.id.btnCancel)   // 취소 버튼
 
+        btnConfirm.setOnClickListener {
+            // 로그아웃 로직 실행 (예: SharedPreferences 삭제, 로그인 화면 이동)
+            dialog.dismiss()
+        }
+
+        btnCancel.setOnClickListener {
+            dialog.dismiss() // 팝업 닫기
+        }
+
+        dialog.setContentView(view)
+        dialog.show()
     }
 
 

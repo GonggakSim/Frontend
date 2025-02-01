99e76241c9d3c641931457ce4b23bea1b66ce4ad
jam-jang <165284582+jam-jang@users.noreply.github.com>
2025-02-01 22:17:12 +0900

    [Feat] Help 액티비티 연결

---
+++ app/src/main/java/com/example/gonggaksim_frontend/HelpActivity.kt
@@ -0,0 +1,34 @@
+package com.example.gonggaksim_frontend
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HelpActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_help)
+
+        val nextButton:ImageButton = findViewById(R.id.HelpbackBtn)
+        val navigatetoMyPageFragment = Intent(this,MypageFragment::class.java)
+        nextButton.setOnClickListener{
+            startActivity(navigatetoMyPageFragment)
+            finish()
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/gonggaksim_frontend/MypageFragment.kt
+++ app/src/main/java/com/example/gonggaksim_frontend/MypageFragment.kt
@@ -1,10 +1,12 @@
 package com.example.gonggaksim_frontend
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 class MypageFragment : Fragment() {
 
@@ -13,5 +15,17 @@ class MypageFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnHelp = view.findViewById<ImageButton>(R.id.tvHelpBtn) // 도움말 버튼 가져오기
+
+        btnHelp.setOnClickListener {
+            val intent = Intent(requireContext(), HelpActivity::class.java)
+            startActivity(intent) // HelpActivity로 이동
+        }
+    }
+
 
 }

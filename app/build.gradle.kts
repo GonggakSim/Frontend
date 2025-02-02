1c2323d1e7fee34bc6eb72cfb9033431c0d172eb
Minseo8170 <163138618+Minseo8170@users.noreply.github.com>
2025-02-02 15:47:54 +0900

    [Feat] api 연동 시도를 위한 retrofit 초기 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation("com.airbnb.android:lottie:5.0.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 }
